56f374edba6d23852484d08cbfc993ec2c09a56c
kumar130 <saurav.tech812@gmail.com>
2019-02-04 17:27:29 +0000

    Changes

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "new"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "new.pub"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
+
