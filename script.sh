88a5fd06a080e82178db18c6de39fbe04c9edb5a
kumar130 <saurav.tech812@gmail.com>
2019-02-04 23:37:28 +0530

    Shell script changes

--- script.sh
+++ script.sh
@@ -2,4 +2,7 @@
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
+sudo apt install openjdk-8-jre -y
 sudo apt-get install jenkins -y
+sudo service jenkins restart
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword >> test.txt
