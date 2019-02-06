658f7e26c55e77a3aa1f1bf6e5b573003c4dd737
kumar130 <saurav.tech812@gmail.com>
2019-02-06 16:18:08 +0000

    Changes to Script to add maven to isntance

--- script.sh
+++ script.sh
@@ -10,3 +10,19 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get install ansible -y
 ansible --version
 
+sudo apt-get update
+sudo apt install maven -y
+mvn -version
+
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo docker version
