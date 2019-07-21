4e38f16e2dd8e1cbd53ab36225d93bdacd0be01b
Saurav Kumar <sauravkumar@Sauravs-MacBook-Air.local>
2019-07-21 18:52:21 +0530

    Key updated

--- instance.tf
+++ instance.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "test" {
+resource "aws_key_pair" "new" {
   key_name = "test"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
@@ -6,7 +6,7 @@ resource "aws_key_pair" "test" {
 resource "aws_instance" "master" {
   ami = "ami-7ea88d1b"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.test.key_name}"
+  key_name = "${aws_key_pair.new.key_name}"
   subnet_id = "${aws_subnet.main-public.id}"
   vpc_security_group_ids = ["${aws_security_group.jmaster.id}"]
   #user_data = "${file("script.sh")}"
